56d43aad3816c5b8499631afe3e30026817d23ff
Damianlysomirski <79919598+Damianlysomirski@users.noreply.github.com>
2023-07-17 09:51:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parrents New Helth Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('bla bla bla')
